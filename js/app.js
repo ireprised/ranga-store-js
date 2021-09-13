37fe385da4da092a22a112a34697b8d933c03ce9
ireprised <ireprised@gmail.com>
2021-09-13 13:29:58 +0600

    1st issue update-image issue

--- js/app.js
+++ js/app.js
@@ -8,8 +8,10 @@ const loadProducts = () => {
 // show all product in UI 
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
+  
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
+    console.log(image);
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
